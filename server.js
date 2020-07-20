1c93929ea41023ea40f5db53cd47c2532dde6b87
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-20 17:43:14 +0700

    fix

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const Express = require('express');
 const Spdy = require('spdy');
 const Https = require('https');
 const { certificate, getFileHttp1 } = require('./shared');
+const simulateLatency = require('express-simulate-latency');
+
+const lag = simulateLatency({ min: 1000, max: 5000 });
 
 // HTTP1
 const http1app = Express();
 http1app.use(Express.static('assets'));
+http1app.use(lag)
 
 // HTTP2
 const http2app = Express();
@@ -28,6 +32,7 @@ http2app.use((req, res) => {
     res.writeHead(200, { 'Content-Type': file.contentType });
     res.end(file.content, 'utf-8');
 });
+http2app.use(lag)
 
 Https.createServer(certificate, http1app).listen(3001, 'localhost', () => {
     console.log("HTTP/1.x")
