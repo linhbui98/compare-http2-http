71070020c4f967d77017ce1039c395e6940e3870
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-22 09:20:19 +0700

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Express = require('express');
 const Spdy = require('spdy');
 const Https = require('https');
+const path = require('path')
 const { certificate, getFileHttp1 } = require('./shared');
 const PORT_HTTP2 = process.env.PORT_HTTP2 || 3000;
 const PORT_HTTP1 = process.env.PORT_HTTP1 || 3001;
@@ -8,11 +9,17 @@ const PORT_HTTP1 = process.env.PORT_HTTP1 || 3001;
 // HTTP1
 const http1app = Express();
 http1app.use(Express.static('assets'));
+http1app.set('views', path.join(__dirname, 'views'));
+http1app.set('view engine', 'pug');
 
 // HTTP2
 const http2app = Express();
+http2app.set('views', path.join(__dirname, 'views'));
+http2app.set('view engine', 'pug');
 http2app.use((req, res) => {
-    const reqPath = req.url === '/' ? '/index2.html' : req.url
+    const host = req.headers.host.split(':')[0];
+    const reqPath = req.url === '/' ? '../views/index.pug' : req.url
+        // const reqPath = req.url === '/' ? 'index2.html' : req.url
     if (req.url === '/') {
         for (let i = 1; i <= 100; i++) {
             const assetPath = `/download (${i}).jpeg`;
@@ -24,13 +31,14 @@ http2app.use((req, res) => {
             stream.end(file.content)
         }
     }
-    const file = getFileHttp1(reqPath)
-    res.writeHead(200, { 'Content-Type': file.contentType });
-    res.end(file.content, 'utf-8');
+    // const file = getFileHttp1(reqPath)
+    // res.writeHead(200, { 'Content-Type': file.contentType });
+    // res.end(file.content, 'utf-8');
+    res.render('index', { title: 'Hey', host: host, PORT_HTTP2: PORT_HTTP2, PORT_HTTP1: PORT_HTTP1 })
 });
 
 Https.createServer(certificate, http1app).listen(PORT_HTTP1, () => {
     Spdy.createServer(certificate, http2app).listen(PORT_HTTP2, () => {
-        console.log(`Running at https://localhost:3000`)
+        console.log(`Running app`)
     });
 });
